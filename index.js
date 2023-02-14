dee47ae7e08958db5bfa42e61736f9c9d2579829
Jithinppp <jithinp968@gmail.com>
2023-02-14 07:37:11 +0530

    changes in value tag

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const value = document.getElementById("value");
 
+value.textContent = `Your screen width is ${window.innerWidth}px`;
 window.addEventListener("resize", (e) => {
   console.log(window.innerWidth);
   value.textContent = `Your screen width is ${window.innerWidth}px`;
